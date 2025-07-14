85b18159adf0b7842f632aa9ecef97b2d46b1fa1
HenricoHosaki <henricohosaki@gmail.com>
2025-07-14 14:32:49 -0300

    Adição de método try catch no código

--- Fundamentos_debug/function_debug/functions_deb.js
+++ Fundamentos_debug/function_debug/functions_deb.js
@@ -1,16 +1,31 @@
 function somar(num1,num2){
+    if(isNaN(num1)||isNaN(num2)){
+        throw new Error("Favor informar somente números");
+    }
     return num1 + num2;
 }
 
-function multiplicar(num1, num2){
+function multiplicar(num1,num2){
+    if(isNaN(num1)||isNaN(num2)){
+        throw new Error("Favor informar somente números");
+    }
     return num1 * num2;
 }
 
-function dividir(num1 , num2){
-    return num1 / num2;
+function dividir(num1,num2){
+    if(isNaN(num1)||isNaN(num2)){
+        throw new Error("Favor informar somente números");
+    }
+    if(num2 == 0){
+        throw new Error ("Não é possível dividir por zero")
+    }
+   return num1 / num2;
 }
 
-function subtrair(num1 , num2){
+function subtrair(num1,num2){
+    if(isNaN(num1)||isNaN(num2)){
+        throw new Error("Favor informar somente números");
+    }
     return num1 - num2;
 }
 module.exports = { somar, multiplicar, dividir, subtrair }
--- Fundamentos_debug/operacoes_debug/operacoes_deb.js
+++ Fundamentos_debug/operacoes_debug/operacoes_deb.js
@@ -1,41 +1,47 @@
 const {somar,multiplicar,dividir,subtrair} = require ('../function_debug/functions_deb')
 
 function apiSomar(req, res){
+        try{
     const {num1  , num2} = req.body;
     const result = somar(num1, num2)
 
-    res.status(200).send
-    ({
-        result: result
-    })
+    res.status(200).send ({ result: result })
+    } catch(Error){
+        res.status(500).send({ msg: Error.message });
+    }
 }
 
  function apiMultiplicar(req, res){
+        try{
     const {num1  , num2} = req.body;
     const result = multiplicar(num1, num2)
 
-    res.status(200).send
-    ({
-        result: result
-    })
+    res.status(200).send ({ result: result })
+    } catch(e){
+        res.status(500).send({ msg: e.message });
+    }
 }
 
 function apiSubtrair(req, res){
+        try{
     const {num1  , num2} = req.body;
     const result = subtrair(num1, num2)
 
-    res.status(200).send
-    ({
-        result: result
-    })
+    res.status(200).send ({ result: result })
+    } catch(e){
+        res.status(500).send({ msg: e.message });
+    }
 }
 
 function apiDividir(req, res){
+        try{
     const {num1  , num2} = req.body;
     const result = dividir(num1, num2)
-    res.status(200).send({
-        result: result
-    })
+
+    res.status(200).send ({ result: result })
+    } catch(e){
+        res.status(500).send({ msg: e.message });
+    }
 }
 
 module.exports = { apiSomar, apiMultiplicar, apiSubtrair, apiDividir }
